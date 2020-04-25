7034d5fdf6d66774efe3baafeaa985be956a5db8
SagarSaxena <sagar.saxena@alumni.stanford.edu>
2020-04-24 22:41:01 -0400

    feat: add navigation script

--- Project5/src/scripts/test_navigation.sh
+++ Project5/src/scripts/test_navigation.sh
@@ -2,11 +2,10 @@
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/RoboNDProjects/Project5/src/my_robot/worlds/building " &
 sleep 5
-xterm -e " roslaunch turtlebot_navigation gmapping_demo.launch " &
+xterm -e " roslaunch turtlebot_navigation amcl_demo.launch " &
 sleep 5 
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
 
 
 
