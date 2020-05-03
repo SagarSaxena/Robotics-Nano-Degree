40fbfb41af6be0c6d01bcd4ea6672b4a916ee331
SagarSaxena <sagar.saxena@alumni.stanford.edu>
2020-05-03 03:20:46 +0000

    feat: Tweak gmapping params and regen map

--- Project5/src/scripts/test_navigation.sh
+++ Project5/src/scripts/test_navigation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/myWorld " &
 sleep 5
-xterm -e " roslaunch turtlebot_navigation amcl_demo.launch " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/map.yaml " &
 sleep 5 
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
