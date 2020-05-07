79e957486f4c7537510cb694cb6f702d78612204
root <sagar.saxena@alumni.stanford.edu>
2020-05-07 02:49:05 +0000

    fix: Correct map orientation and robot start pose

--- Project5/src/scripts/pick_objects.sh
+++ Project5/src/scripts/pick_objects.sh
@@ -6,5 +6,5 @@ sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/map.yaml " &
 sleep 5 
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-#sleep 5
-#xterm -e " rosrun pick_objects pick_objects "
+sleep 5
+xterm -e " rosrun pick_objects pick_objects "
