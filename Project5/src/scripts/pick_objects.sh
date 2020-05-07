7ddb49e53e47c69fda2445a4eba495192eb02948
root <sagar.saxena@alumni.stanford.edu>
2020-05-07 02:48:47 +0000

    fix: Correct map orientation and robot start pose

--- Project5/src/scripts/pick_objects.sh
+++ Project5/src/scripts/pick_objects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ROBOT_INITIAL_POSE="-x 1 -y -1.5 -Y 1.57"
+export ROBOT_INITIAL_POSE="-x 4 -y 1.5 -Y 1.57"
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/myWorld " &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/map.yaml " &
