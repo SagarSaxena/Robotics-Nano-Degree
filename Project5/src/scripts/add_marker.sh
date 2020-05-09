5c73905e6130fd4ff6cf8e3f30bf61897fab56ac
root <sagar.saxena@alumni.stanford.edu>
2020-05-09 03:15:02 +0000

    feat: Implment add_marker package to display goalPose

---
+++ Project5/src/scripts/add_marker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export ROBOT_INITIAL_POSE="-x 4 -y 1.5 -Y 1.57"
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/myWorld " &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/map.yaml " &
+sleep 5 
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm -e " rosrun add_markers add_markers "
