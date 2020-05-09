19e88911796e763d431ac18132fa1bb6add217a7
root <sagar.saxena@alumni.stanford.edu>
2020-05-09 02:43:51 +0000

    Feat: Implement navigation to multiple goal points

--- Project5/src/pick_objects/src/pick_objects.cpp
+++ Project5/src/pick_objects/src/pick_objects.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <tf/tf.h>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -11,6 +12,10 @@ int main(int argc, char** argv){
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
+
+  // initialize goal positions
+  float goalPose[2][3] = {{-1, 1.75, 0}, {-0.75, -3.5, 1.57}};
+  int numGoals = 2; 
 
   // Wait 5 sec for move_base action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
@@ -23,24 +28,31 @@ int main(int argc, char** argv){
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = -1.0;
-  goal.target_pose.pose.position.y = 1.75;
+  for (int i = 0; i < numGoals; i++) {
 
-  goal.target_pose.pose.orientation.w = 1.0;
+    // Define goal pose
+    goal.target_pose.pose.position.x = goalPose[i][0];
+    goal.target_pose.pose.position.y = goalPose[i][1];
+    goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(goalPose[i][2]);
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+    // Send the goal pose of the pickup zone
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
 
-  // Wait an infinite time for the results
-  ac.waitForResult();
+    // Wait an infinite time for the results
+    ac.waitForResult();
 
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    // Check if the robot reached its goal
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Successfully reached pick up zone");
+
+      ros::Duration(5.0).sleep();
+    }
+    else {
+      ROS_INFO("Robot failed to reach pick up zone");
+    }
+
+  } 
 
   return 0;
 }
